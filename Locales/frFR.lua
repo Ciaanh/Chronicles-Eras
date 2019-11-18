335be47d2e9aa9d03aa91c7b9f996f9d4af254a8
Nicolas Fabre <nicolas.fabre.21@outlook.fr>
2019-11-18 22:31:08 +0100

    Fix bugs

--- Locales/frFR.lua
+++ Locales/frFR.lua
@@ -1,10 +1,12 @@
-local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
-local L = AceLocale:NewLocale("Chronicles - Eras", "frFR", false);
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("Chronicles - Eras", "frFR", false)
 
-if not L then return end
+if not L then
+    return
+end
 
 if L then
     L["Black Empire label"] = "L'empire noir"
-    L["Black Empire page 1"] = "<HTML><BODY><BR/><BR/><H1 align=\"center\">L'empire noir page 1</H1></BODY></HTML>"
-    L["Black Empire page 2"] = "<HTML><BODY><BR/><BR/><H1 align=\"center\">L'empire noir page 2</H1></BODY></HTML>"
+    L["Black Empire page 1"] = '<HTML><BODY><BR/><BR/><H1 align="center">L\'empire noir page 1</H1></BODY></HTML>'
+    L["Black Empire page 2"] = '<HTML><BODY><BR/><BR/><H1 align="center">L\'empire noir page 2</H1></BODY></HTML>'
 end
